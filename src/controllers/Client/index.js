8dfc4bc921f13fa2973260e4e49040499dc99701
lovecpu <lovecpu@cndn.co.kr>
2025-02-26 04:22:00 +0000

    [style] name => id

--- src/controllers/Client/index.js
+++ src/controllers/Client/index.js
@@ -316,7 +316,7 @@ const getPostsDetail = (req, res, next) => {
         )
     ) 
     FROM userList 
-    WHERE posts.author = userList.name) AS users  -- 비교 대상 수정
+    WHERE posts.author = userList.id) AS users  -- 비교 대상 수정
 FROM 
     posts
 WHERE 
