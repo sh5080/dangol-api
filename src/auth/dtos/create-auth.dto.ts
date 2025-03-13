7b7b47337d90e94904db35d8d689207314d9e163
prpn97 <prpn97@gmail.com>
2025-03-13 15:44:39 +0900

    feat: 공통 로그인 인터페이스, 확장

--- src/auth/dtos/create-auth.dto.ts
+++ src/auth/dtos/create-auth.dto.ts
@@ -1,13 +1,16 @@
 import { User } from "@prisma/client";
 import { tags } from "typia";
+import { AuthProviderType } from "../../types/enum.type";
 
-export interface LoginDto {
+interface EqualLoginDto {
   email: string & tags.Format<"email">;
+  authType: AuthProviderType;
+}
+
+export interface LoginDto extends EqualLoginDto {
   password: string & tags.MinLength<8> & tags.MaxLength<16>;
 }
 
-export interface SocialLoginDto {
-  email: string & tags.Format<"email">;
-}
+export interface SocialLoginDto extends EqualLoginDto {}
 
 export interface UserWithoutPassword extends Omit<User, "password"> {}
