a724a0b6480f53cd4802959e981142a08d637d23
prpn97 <prpn97@gmail.com>
2025-03-13 13:57:22 +0900

    feat: 토큰 검증 api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Req } from "@nestjs/common";
+import { Controller, Req, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto, SocialLoginDto } from "./dtos/create-auth.dto";
 import { TypedBody, TypedRoute } from "@nestia/core";
 import { ApiTags } from "@nestjs/swagger";
 import { Request } from "express";
+import { AuthGuard } from "./auth.guard";
 
 @ApiTags("인증")
 @Controller("auth")
@@ -11,7 +12,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   /**
-   * @summary 로그인
+   * @summary 로그인 (완료)
    * @param dto 로그인 dto
    * @param req 인증 요청
    * @returns 인증 결과 (accessToken, refreshToken은 response header, cookie에 포함)
@@ -34,5 +35,16 @@ export class AuthController {
     const ip = req.ip as string;
     const userAgent = req.headers["user-agent"] as string;
     return this.authService.authenticate(dto, ip, userAgent);
+  }
+
+  /**
+   * @summary 토큰 검증
+   * @security bearer
+   * @returns 토큰 검증 결과 true/false
+   */
+  @TypedRoute.Get("token")
+  @UseGuards(AuthGuard)
+  async token() {
+    return true;
   }
 }
