cf50867d986c59213f2c564a56d9681b25db0c83
prpn97 <prpn97@gmail.com>
2025-03-14 16:25:34 +0900

    chore: equal >> base 네이밍 수정

--- src/auth/dtos/create-auth.dto.ts
+++ src/auth/dtos/create-auth.dto.ts
@@ -2,18 +2,18 @@ import { User } from "@prisma/client";
 import { tags } from "typia";
 import { AuthProviderType } from "../../types/enum.type";
 
-interface EqualLoginDto {
+interface BaseLoginDto {
   email: string & tags.Format<"email">;
   authType: AuthProviderType;
 }
 
-export interface LoginDto extends EqualLoginDto {
+export interface LoginDto extends BaseLoginDto {
   password: string &
     tags.MinLength<8> &
     tags.MaxLength<16> &
     tags.Example<"test123123!">;
 }
 
-export interface SocialLoginDto extends EqualLoginDto {}
+export interface SocialLoginDto extends BaseLoginDto {}
 
 export interface UserWithoutPassword extends Omit<User, "password"> {}
