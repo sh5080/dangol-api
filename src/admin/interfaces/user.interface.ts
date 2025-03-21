4d035647b601572fa648c3b25d0b4b7a42cea15a
prpn97 <prpn97@gmail.com>
2025-03-21 14:17:55 +0900

    fix: pagination dto로 통일함

--- src/admin/interfaces/user.interface.ts
+++ src/admin/interfaces/user.interface.ts
@@ -1,9 +1,9 @@
-import { GetUserListDto } from "../user/dtos/get-user.dto";
+import { PaginationDto } from "../../common/dtos/common.dto";
 import { UserDetail } from "../user/dtos/response.dto";
 import { UpdateUserPermissionDto } from "../user/dtos/update-user.dto";
 
 export interface IUserService {
-  getUserList(dto: GetUserListDto): Promise<UserDetail[]>;
+  getUserList(dto: PaginationDto): Promise<UserDetail[]>;
   getUserDetail(id: string): Promise<UserDetail>;
   updateUserPermission(dto: UpdateUserPermissionDto): Promise<void>;
 }
