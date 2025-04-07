eda83918755ee4bac884eed6ef38744ce70b22db
prpn97 <prpn97@gmail.com>
2025-04-07 13:08:59 +0900

    remove: 그라파나 클라우드 연동관련 레거시 제거

--- src/metrics/metrics.service.ts
+++ src/metrics/metrics.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { Counter, Histogram, register, Registry } from "prom-client";
 import { MetricConfig } from "../types/data.type";
-import { env } from "../configs/env.config";
 
 @Injectable()
 export class MetricsService implements OnModuleInit {
@@ -19,20 +18,6 @@ export class MetricsService implements OnModuleInit {
       labelNames: ["method", "route", "status_code"],
       registers: [this.registry],
     });
-
-    // Grafana Cloud Prometheus 설정
-    if (env.GRAFANA_CLOUD_USER && env.GRAFANA_CLOUD_API_KEY) {
-      const { setupPushgateway } = require("prom-client");
-      setupPushgateway({
-        url: "https://prometheus-prod-10-prod-us-central-0.grafana.net/api/prom/push",
-        basicAuth: {
-          username: env.GRAFANA_CLOUD_USER,
-          password: env.GRAFANA_CLOUD_API_KEY,
-        },
-        jobName: "jdg-api",
-        interval: 15000, // 15초
-      });
-    }
   }
 
   onModuleInit() {
