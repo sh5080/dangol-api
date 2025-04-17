e91ff6c2fcab6a669592c3791592a5c42c21f954
prpn97 <prpn97@gmail.com>
2025-04-17 16:14:08 +0900

    feat: 암/복호화 공통함수 적용

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -32,148 +32,104 @@ export class PrismaService implements OnModuleInit, OnModuleDestroy {
     });
     const encryption = this.encryptionService;
     const prismaInstance = this._prisma;
+    const encryptUserData = async (data: any) => {
+      return await encryption.encrypt(data, env.ENCRYPTION_KEY);
+    };
+    const decryptUserData = async (data: any) => {
+      return await encryption.decrypt(data, env.ENCRYPTION_KEY);
+    };
 
     this.prisma = this._prisma.$extends({
       model: {
         user: {
           async create(params: Prisma.UserCreateArgs) {
-            const { data } = params;
-
-            if (data.email) {
-              data.email = await encryption.encrypt(
-                data.email,
-                env.ENCRYPTION_KEY
-              );
+            if (params.data?.email) {
+              params.data.email = await encryptUserData(params.data.email);
             }
-            if (data.phoneNumber) {
-              data.phoneNumber = await encryption.encrypt(
-                data.phoneNumber,
-                env.ENCRYPTION_KEY
+            if (params.data?.phoneNumber) {
+              params.data.phoneNumber = await encryptUserData(
+                params.data.phoneNumber
               );
             }
 
             const user = await prismaInstance.user.create(params);
-            user.email = await encryption.decrypt(
-              user.email,
-              env.ENCRYPTION_KEY
-            );
-
-            user.phoneNumber = await encryption.decrypt(
-              user.phoneNumber,
-              env.ENCRYPTION_KEY
-            );
+            if (user.email) {
+              user.email = await decryptUserData(user.email);
+            }
+            if (user.phoneNumber) {
+              user.phoneNumber = await decryptUserData(user.phoneNumber);
+            }
 
             delete (user as any).password;
             return user;
           },
           async findUnique(params: Prisma.UserFindUniqueArgs) {
             if (params.where.email) {
-              params.where.email = await encryption.encrypt(
-                params.where.email,
-                env.ENCRYPTION_KEY
-              );
+              params.where.email = await encryptUserData(params.where.email);
             }
             if (params.where.phoneNumber) {
-              params.where.phoneNumber = await encryption.encrypt(
-                params.where.phoneNumber,
-                env.ENCRYPTION_KEY
+              params.where.phoneNumber = await encryptUserData(
+                params.where.phoneNumber
               );
             }
             const result = await prismaInstance.user.findUnique(params);
             if (result && result.email) {
-              result.email = await encryption.decrypt(
-                result.email,
-                env.ENCRYPTION_KEY
-              );
+              result.email = await decryptUserData(result.email);
             }
             if (result && result.phoneNumber) {
-              result.phoneNumber = await encryption.decrypt(
-                result.phoneNumber,
-                env.ENCRYPTION_KEY
-              );
+              result.phoneNumber = await decryptUserData(result.phoneNumber);
             }
             return result;
           },
           async findFirst(params: Prisma.UserFindFirstArgs) {
             if (params.where?.email) {
-              params.where.email = await encryption.encrypt(
-                params.where.email as string,
-                env.ENCRYPTION_KEY
-              );
+              params.where.email = await encryptUserData(params.where.email);
             }
             const result = await prismaInstance.user.findFirst(params);
             if (result && result.email) {
-              result.email = await encryption.decrypt(
-                result.email,
-                env.ENCRYPTION_KEY
-              );
+              result.email = await decryptUserData(result.email);
             }
             if (result && result.phoneNumber) {
-              result.phoneNumber = await encryption.decrypt(
-                result.phoneNumber,
-                env.ENCRYPTION_KEY
-              );
+              result.phoneNumber = await decryptUserData(result.phoneNumber);
             }
             return result;
           },
           async findMany(params: Prisma.UserFindManyArgs) {
             if (params?.where?.email) {
-              params.where.email = await encryption.encrypt(
-                params.where.email as string,
-                env.ENCRYPTION_KEY
-              );
+              params.where.email = await encryptUserData(params.where.email);
             }
             const results = await prismaInstance.user.findMany(params);
             for (const result of results) {
               if (result.email) {
-                result.email = await encryption.decrypt(
-                  result.email,
-                  env.ENCRYPTION_KEY
-                );
+                result.email = await decryptUserData(result.email);
               }
               if (result.phoneNumber) {
-                result.phoneNumber = await encryption.decrypt(
-                  result.phoneNumber,
-                  env.ENCRYPTION_KEY
-                );
+                result.phoneNumber = await decryptUserData(result.phoneNumber);
               }
             }
             return results;
           },
           async count(params: Prisma.UserCountArgs) {
             if (params?.where?.email) {
-              params.where.email = await encryption.encrypt(
-                params.where.email as string,
-                env.ENCRYPTION_KEY
-              );
+              params.where.email = await encryptUserData(params.where.email);
             }
             return prismaInstance.user.count(params);
           },
           async update(params: Prisma.UserUpdateArgs) {
             if (params?.where?.email) {
-              params.where.email = await encryption.encrypt(
-                params.where.email as string,
-                env.ENCRYPTION_KEY
-              );
+              params.where.email = await encryptUserData(params.where.email);
             }
             if (params?.where?.phoneNumber) {
-              params.where.phoneNumber = await encryption.encrypt(
-                params.where.phoneNumber as string,
-                env.ENCRYPTION_KEY
+              params.where.phoneNumber = await encryptUserData(
+                params.where.phoneNumber
               );
             }
             const result = await prismaInstance.user.update(params);
             if (result.email) {
-              result.email = await encryption.decrypt(
-                result.email,
-                env.ENCRYPTION_KEY
-              );
+              result.email = await decryptUserData(result.email);
             }
             if (result.phoneNumber) {
-              result.phoneNumber = await encryption.decrypt(
-                result.phoneNumber,
-                env.ENCRYPTION_KEY
-              );
+              result.phoneNumber = await decryptUserData(result.phoneNumber);
             }
             delete (result as any).password;
             return result;
